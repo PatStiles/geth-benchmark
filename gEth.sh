fd048c53857941d95337dfdb7030d0d4a99dd273
PatStiles <pdstiles@wisc.edu>
2022-02-26 20:26:54 -0600

    tar

--- gEth.sh
+++ gEth.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+cd /local/repository/
 #Install Go note must have downleaded tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
+wget -c https://golang.org/dl/go1.17.7.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.17.7.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 go version
 
@@ -12,12 +14,9 @@ sudo apt-get install ethereum
 
 #Check if Geth installed
 geth
-
 #Make Data Cache
 mkdir data
 
 #Initialize geth node 
-geth init ./genesis.json
-geth --datadir /data --allow-insecure-unlock --password "" account new
-geth --allow-insecure-unlock --rpc --http --ws --networkid 666 --datadir /data  init genesis.json
-
+nohup geth --mine --minerthreads 1 --allow-insecure-unlock --rpc --http --ws --networkid 666 --datadir /data init genesis.json &
+geth attach --exec "eth.blockNumber"
